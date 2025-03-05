ad1b158bdde236391713f60e4a29ef0157b665ca
Mederic Fauvernier <mederic.fauvernier@gmail.com>
2025-03-05 14:29:42 +0100

    RequireAuth corrected

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,6 +1,7 @@
+import { JSX } from "react";
 import { Navigate } from "react-router-dom";
 
-const RequireAuth = ({ children }: { children: JSX.element }) => {
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/users/login" />;
 };
